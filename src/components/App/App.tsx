635087577159973cbd0118e42f3353625e409967
Behnam <behnam.tehrani@turbinekreuzberg.com>
2020-12-24 10:53:27 +0100

    setting up - tailwindcss - sass - prettier - eslint

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="app">
+      <header className="app__header">BREAD</header>
+      <main className="app__main">GIVE ME</main>
+    </div>
+  );
+};
+
+export default App;
