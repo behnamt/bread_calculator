73854aafc3c8bad5a93ee05df4a206f05fb2bdbd
Behnam <behnam.tehrani@turbinekreuzberg.com>
2020-12-31 15:26:34 +0100

    add translation using react-i18next

--- src/components/organisms/AddBreadForm/AddBreadForm.tsx
+++ src/components/organisms/AddBreadForm/AddBreadForm.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useOrbitDB } from '../../../context/orbit-db';
 import Input from '../../atoms/Input/Input';
 
 const BreadForm: React.FC = () => {
   const { DB } = useOrbitDB();
+  const { t } = useTranslation('common');
 
   const [name, setName] = useState<string>('');
   const [alt, setAlt] = useState<string>('');
 
-  const onSubmit = () => DB!.put({ name, alt });
+  const onSubmit = () => DB?.put({ name, alt });
 
   return (
     <div className="mt-5 md:mt-0 md:col-span-2">
@@ -18,10 +20,18 @@ const BreadForm: React.FC = () => {
             <div className="px-4 py-5 bg-white sm:p-6">
               <div className="grid grid-cols-6 gap-6">
                 <div className="col-span-6 sm:col-span-3">
-                  <Input label="Name" name="name" onChange={setName} />
+                  <Input
+                    label={t('bread-form.name')}
+                    name="name"
+                    onChange={setName}
+                  />
                 </div>
                 <div className="col-span-6 sm:col-span-3">
-                  <Input label="Alt" name="alt" onChange={setAlt} />
+                  <Input
+                    label={t('bread-form.alt')}
+                    name="alt"
+                    onChange={setAlt}
+                  />
                 </div>
               </div>
               <button onClick={onSubmit} type="button">
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
 import App from './components/organisms/App/App';
 import { OrbitDBProvider } from './context/orbit-db';
+import commonEn from './translations/en/common.json';
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: 'en',
+  resources: {
+    en: {
+      common: commonEn,
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <OrbitDBProvider>
-      <App />
-    </OrbitDBProvider>
+    <I18nextProvider i18n={i18next}>
+      <OrbitDBProvider>
+        <App />
+      </OrbitDBProvider>
+    </I18nextProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
